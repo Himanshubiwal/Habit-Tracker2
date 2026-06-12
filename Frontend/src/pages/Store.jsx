c417c3576a7d55eabdadab949a21ce7dbe5350d8
himanshubinwal <24ce01034@iitbbs.ac.in>
2026-06-12 16:38:46 +0530

    restructured folders

---
+++ Frontend/src/pages/Store.jsx
@@ -0,0 +1,21 @@
+import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
+import { useState } from "react";
+
+const Store = () => {
+  const [Open, setOpen] = useState(true);
+  function change() {
+    const newOpen = !Open;
+    setOpen(newOpen);
+  }
+  return (
+    <div className="bg-[#2b2d38] flex flex-row p-4 gap-2">
+      <Sidebar open={Open} />
+      <div className="h-screen flex-1 gap-2 flex flex-col ">
+        <Navbar fnc={change} name="Store" />
+      </div>
+    </div>
+  );
+};
+
+export default Store
