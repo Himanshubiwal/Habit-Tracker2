75e515663e2e93e17b6e0cbcb60aea08cd854c2a
himanshubinwal <24ce01034@iitbbs.ac.in>
2026-07-05 21:44:15 +0530

    fixed api base url

--- Frontend/src/api/auth.js
+++ Frontend/src/api/auth.js
@@ -1,56 +1,81 @@
-import axios from "axios";
+import axios  from "axios";
 import { useContext } from "react";
 import { HabitContext } from "../context/HabitDataProvider";
 
-async function RegisterUser(data) {
-  try {
-    if (!data.username) {
-      throw new Error("username is required");
-    }
-    if (!data.email) {
-      throw new Error("email is required");
-    }
-    if (!data.password) {
-      throw new Error("password is required");
-    }
+
+
+
+ async function RegisterUser(data){
+
+
+
+
+
+    try {
+
+
+
+        if(!data.username){
+            throw new Error("username is required")
+        }
+        if(!data.email){
+            throw new Error ("email is required")
+        }
+        if(!data.password){
+            throw new Error("password is required")
+        }
+
 
     const response = await axios.post(
-      "http://localhost:3000/api/auth/register",
+      "https://habit-tracker2-1.onrender.com/api/auth/register",
       data,
       {
-        withCredentials: true,
-      },
+        withCredentials: true
+      }
     );
     console.log(response.data);
     return true;
   } catch (error) {
     throw error.response.data;
-  }
+    
+  } 
+  
 }
 
-async function LoginUser(data) {
-  try {
-    if (!data.username && !data.username) {
-      throw new Error("username is required");
-    }
 
-    if (!data.password) {
-      throw new Error("password is required");
-    }
 
-    const response = await axios.post(
-      "https://habit-tracker2-1.onrender.com/api/auth/login",
-      data,
-      {
-        withCredentials: true,
-      },
-    );
+async function LoginUser(data){
 
-    localStorage.setItem("email", response.data.user.email);
-    console.log(response.data);
-  } catch (error) {
+
+
+
+  try{
+        if(!data.username && !data.username){
+            throw new Error("username is required")
+        }
+
+        if(!data.password){
+            throw new Error("password is required")
+        }
+
+        const response = await axios.post(
+          "https://habit-tracker2-1.onrender.com/api/auth/login"
+          ,data,
+        {
+          withCredentials:true
+        }
+      );
+
+
+      localStorage.setItem("email",response.data.user.email)
+        console.log(response.data)
+
+  }
+  catch(error){
     throw error.response.data;
   }
 }
 
-export { LoginUser, RegisterUser };
+
+
+export  {LoginUser,RegisterUser};
